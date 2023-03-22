ce2dab4425cdcb885d137c7b8424dd2caa05d1e6
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-03-22 01:36:51 -0400

    update

--- src/main/java/leetcode/T202IsHappy.java
+++ src/main/java/leetcode/T202IsHappy.java
@@ -1,14 +1,14 @@
 /**
  * 202. Happy Number
  * Write an algorithm to determine if a number n is happy.
- *
+ * /
  * A happy number is a number defined by the following process:
  * Starting with any positive integer, replace the number by the sum of the squares of its digits.
  * Repeat the process until the number equals 1 (where it will stay),
  * or it loops endlessly in a cycle which does not include 1.
  * Those numbers for which this process ends in 1 are happy.
  * Return true if n is a happy number, and false if not.
- *
+ * /
  * Example 1:
  * Input: n = 19
  * Output: true
@@ -35,7 +35,6 @@ public class T202IsHappy {
     }
 
     public static boolean isHappy(int n) {
-        boolean res;
         int step = sumOfDigitSqr(n);
         Set<Integer> record = new HashSet<>();
         while (step != 1 && !record.contains(step)){
@@ -46,6 +45,6 @@ public class T202IsHappy {
     }
 
     public static void main(String[] args) {
-        System.out.println(isHappy(19));;
+        System.out.println(isHappy(19));
     }
 }
