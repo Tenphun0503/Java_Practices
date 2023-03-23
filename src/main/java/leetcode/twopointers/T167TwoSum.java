d2a013cb4029d130c048478bd13a58a486463591
tenphun0503 <tenzinphuntsok0503@gmail.com>
2023-03-22 21:27:31 -0400

    update

---
+++ src/main/java/leetcode/twopointers/T167TwoSum.java
@@ -0,0 +1,8 @@
+/**
+ * 167. Two Sum II - Input Array Is Sorted
+ * {@link https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/}
+ */
+package leetcode.twopointers;
+
+public class T167TwoSum {
+}
