af63b69b92337d64517c315504a570113174f279
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-03-25 15:38:38 -0400

    update

--- src/main/java/leetcode/twopointers/T524FindLongestWord.java
+++ src/main/java/leetcode/twopointers/T524FindLongestWord.java
@@ -1,33 +1,71 @@
 /**
  * 524. Longest Word in Dictionary through Deleting
+ *
  * @see <a href="https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/"></a>
  */
 package leetcode.twopointers;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class T524FindLongestWord {
+    /**
+     * To see if a dic is substring of s
+     */
+    public static boolean findWord(String s, String dic) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < dic.length()) {
+            if (s.charAt(i) != dic.charAt(j)) {
+                i++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+        return j == dic.length();
+    }
+
+    /**
+     * first sort the dictionary and then look for first one result
+     */
     public static String findLongestWord(String s, List<String> dictionary) {
-         Collections.sort(dictionary, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                if (s1.length() != s2.length()) {
-                    return s2.length() - s1.length(); // Longer strings come first
-                } else {
-                    return s1.compareTo(s2); // Sort by lexicographical order
-                }
+        Collections.sort(dictionary, (s1, s2) -> {
+            if (s1.length() != s2.length()) {
+                return s2.length() - s1.length(); // Longer strings come first
+            } else {
+                return s1.compareTo(s2); // Sort by lexicographical order
             }
         });
-        System.out.println(dictionary);
+        for (String dic : dictionary) {
+            boolean res = findWord(s, dic);
+            if (res) return dic;
+        }
         return "";
+    }
+
+    /**
+     * compare all in dictionary that fits more than longestWord
+     * Better on time complexity
+     */
+    public static String findLongestWord2(String s, List<String> dictionary) {
+        String longestword = "";
+        for (String dic : dictionary) {
+            int l1 = longestword.length(), l2 = dic.length();
+            if (l1 > l2 || l1 == l2 && longestword.compareTo(dic) < 0) {
+                continue;
+            }
+            if (findWord(s, dic)) {
+                longestword = dic;
+            }
+        }
+        return longestword;
     }
 
     public static void main(String[] args) {
         String s = "abpcplea";
-        List<String> dictionary = Arrays.asList("ale","apple","monkey","plea");
+        List<String> dictionary = Arrays.asList("ale", "apple", "monkey", "plea");
         System.out.println(findLongestWord(s, dictionary));
+        System.out.println(findLongestWord2(s, dictionary));
     }
 }
