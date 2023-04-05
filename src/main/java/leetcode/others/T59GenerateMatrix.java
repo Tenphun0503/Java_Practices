10bcb57b6d92c52a619f9e06bd4d921f0c4b787d
tenphun0503 <tenzinphuntsok0503@gmail.com>
2023-04-05 18:42:57 -0400

    update

--- src/main/java/leetcode/others/T59GenerateMatrix.java
+++ src/main/java/leetcode/others/T59GenerateMatrix.java
@@ -12,21 +12,21 @@ public class T59GenerateMatrix {
     /**
      * We build the matrix layer by layer from the out to inner.
      * for example: for the matrix below
-     *      1	2	3	4	5
-     *      16	17	18	19	6
-     *      15	24	25	20	7
-     *      14	23	22	21	8
-     *      13	12	11	10	9
+     *     1	2	3	4	5
+     *     16	17	18	19	6
+     *     15	24	25	20	7
+     *     14	23	22	21	8
+     *     13	12	11	10	9
      * we build 1-16, then 17-24, then 25: which is, build an n=5 circle, then n=3, then n=1; (n-=2)
      * for each layer, we need to determine something
      *      1. the size `n`, e.g. n = 5
      *      2. the index of top-left element, which is the start of the matrix `start`, e.g. start = 0
      *      3. the starting value `last`, e.g. last = 1
-     *       1	2	3	4	5
-     *       16	        	6
-     *       15	        	7
-     *       14	        	8
-     *       13	12	11	10	9
+     *      1	2	3	4	5
+     *      16	        	6
+     *      15	        	7
+     *      14	        	8
+     *      13	12	11	10	9
      * We can build each layer like this:
      *      1. for M[0][0:n-1]      we put  (0:n-1)+last
      *      2. for M[0:n-1][n-1]    we put  (n-1:2n-2)+last
