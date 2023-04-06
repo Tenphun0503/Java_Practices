9a431890ffa27fc6b4abeeaf9645911f5978cb9c
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-04-06 18:04:53 -0400

    Rebuild the linkedlist

--- src/main/java/leetcode/datastructure/LinkedList.java
+++ src/main/java/leetcode/datastructure/LinkedList.java
@@ -1,33 +1,103 @@
 /**
- * This class implement class of linked list
- * TODO: Insertion, Deletion, Contains,
+ * LinkedList.java
+ * Author: Ten phun
+ * This class implement single linked list
+ * Methods:
+ * GetHead(), GetSize(), isEmpty(), clean(), get(i), addAt(i, v), addAtHead(v), addAtTail(v), deleteAt(i), buildAsList(list)
  */
 package leetcode.datastructure;
 
 public class LinkedList {
-    public ListNode head;
-    private int length;
+    private final ListNode head;
+    private int size;   // sentinel node as pseudo-head
 
     public LinkedList() {
-        this.head = null;
+        size = 0;
+        head = new ListNode(0);
     }
 
-    public LinkedList(int val) {
-        this.head = new ListNode(val);
+    public ListNode getHead() {
+        return head.next;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clean() {
+        head.next = null;
+        size = 0;
+    }
+
+    /**
+     * Get the value of the index-th node in the linkedList. if the index is invalid, return -1
+     */
+    public int get(int index) {
+        // if index is invalid
+        if (index < 0 || index >= size) return -1;
+
+        ListNode curr = head;
+        for (int i = 0; i < index + 1; i++) curr = curr.next;
+        return curr.val;
+    }
+
+    /**
+     * Add a node of value val before the index-th node in the linked list.
+     * If index equals to the length of linked list,
+     * the node will be appended to the end of linked list.
+     * If index is greater than the length, the node will not be inserted.
+     */
+    public boolean addAt(int index, int val) {
+        if (index > size) return false;
+        if (index < 0) index = 0;
+
+        size++;
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) pred = pred.next;
+        ListNode toAdd = new ListNode(val);
+        toAdd.next = pred.next;
+        pred.next = toAdd;
+        return true;
+    }
+
+    public boolean deleteAt(int index) {
+        // if the index is invalid, do nothing
+        if (index < 0 || index >= size) return false;
+
+        size--;
+        // find predecessor of the node to be deleted
+        ListNode pred = head;
+        for (int i = 0; i < index; ++i) pred = pred.next;
+        // delete pred.next
+        pred.next = pred.next.next;
+
+        return true;
+    }
+
+    public boolean addAtHead(int val) {
+        return addAt(0, val);
+    }
+
+    public boolean addAtTail(int val) {
+        return addAt(size, val);
     }
 
     /**
      * build the linked list according to the int list
-     * @param list: input
      */
     public void buildAsList(int[] list) {
-        this.length = list.length;
-        this.head = new ListNode(list[0]);
-        ListNode current = this.head;
-        for (int i = 1; i < this.length; i++) {
-            ListNode node = new ListNode(list[i]);
-            current.next = node;
-            current = current.next;
+        // clean the list first
+        clean();
+
+        size = list.length;
+        ListNode curr = head;
+        for (int i = 0; i < size; i++) {
+            curr.next = new ListNode(list[i]);
+            curr = curr.next;
         }
     }
 
@@ -35,20 +105,20 @@ public class LinkedList {
     public String toString() {
         StringBuilder res = new StringBuilder();
         res.append("LinkedList{");
-        ListNode current = this.head;
-        while (current != null) {
-            res.append(current.val);
-            res.append(", ");
-            current = current.next;
+
+        ListNode curr = head.next;
+        while (curr != null) {
+            res.append(curr.val).append(", ");
+            curr = curr.next;
         }
         res.append("Length:");
-        res.append(this.length);
+        res.append(size);
         res.append("}");
         return res.toString();
     }
 
     public static void main(String[] args) {
-        int[] list = {3,2,0,-4};
+        int[] list = {3, 2, 0, -4};
         LinkedList linkedList = new LinkedList();
         linkedList.buildAsList(list);
         System.out.println(linkedList);
--- src/main/java/leetcode/datastructure/ListNode.java
+++ src/main/java/leetcode/datastructure/ListNode.java
@@ -5,17 +5,25 @@ package leetcode.datastructure;
 
 public class ListNode {
     public int val;
+    public ListNode prev;
     public ListNode next;
 
     public ListNode(int val) {
         this.val = val;
+        this.prev = null;
         this.next = null;
     }
 
     @Override
     public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.val).append(" ");
+        ListNode node = this.next;
+        while(node!=null){
+            sb.append(node.val).append(" ");
+            node = node.next;
+        }
+
+        return sb.toString();
     }
 }
