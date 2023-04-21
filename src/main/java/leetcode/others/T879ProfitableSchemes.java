9201291370ef4ba228177e99229e13b30a5431d8
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-04-21 01:40:57 -0400

    update

---
+++ src/main/java/leetcode/others/T879ProfitableSchemes.java
@@ -0,0 +1,52 @@
+/**
+ * 879. Profitable Schemes
+ * @see <a href="https://leetcode.com/problems/profitable-schemes/"></a>
+ */
+package leetcode.others;
+
+import java.util.Arrays;
+
+public class T879ProfitableSchemes {
+    int mod = 1000000007;
+    int[][][] memo = new int[101][101][101];
+
+    int find(int pos, int count, int profit, int n, int minProfit, int[] group, int[] profits) {
+        if (pos == group.length) {
+            // If profit exceeds the minimum required; it's a profitable scheme.
+            return profit >= minProfit ? 1 : 0;
+        }
+
+        if (memo[pos][count][profit] != -1) {
+            // Repeated subproblem, return the stored answer.
+            return memo[pos][count][profit];
+        }
+
+        // Ways to get a profitable scheme without this crime.
+        int totalWays = find(pos + 1, count, profit, n, minProfit, group, profits);
+        if (count + group[pos] <= n) {
+            // Adding ways to get profitable schemes, including this crime.
+            totalWays += find(pos + 1, count + group[pos], Math.min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
+        }
+
+        return memo[pos][count][profit] = totalWays % mod;
+    }
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        // Initializing all states as -1.
+        for (int i = 0; i <= group.length; i++) {
+            for(int j = 0; j <= n; j++) {
+                Arrays.fill(memo[i][j], -1);
+            }
+        }
+
+        return find(0, 0, 0, n, minProfit, group, profit);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int minProfit = 3;
+        int[] group = {2,2};
+        int[] profit = {2,3};
+        T879ProfitableSchemes schemes = new T879ProfitableSchemes();
+        System.out.println(schemes.profitableSchemes(n, minProfit, group, profit));
+    }
+}
