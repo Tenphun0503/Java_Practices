d2c0dcc2ae225dd93298f2d0af40fa362db35f0d
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-04-22 15:54:34 -0400

    update

---
+++ src/main/java/leetcode/others/T1312MinInsertions.java
@@ -0,0 +1,40 @@
+/**
+ * 1312. Minimum Insertion Steps to Make a String Palindrome
+ *
+ * @see <a href="https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/"></a>
+ */
+package leetcode.others;
+
+public class T1312MinInsertions {
+    private static int lcs(String s1, String s2, int m, int n, int[][] memo) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        if (memo[m][n] != -1) {
+            return memo[m][n];
+        }
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+        }
+        return memo[m][n] = Math.max(lcs(s1, s2, m - 1, n, memo), lcs(s1, s2, m, n - 1, memo));
+    }
+
+    public static int minInsertions(String s) {
+        int n = s.length();
+        String sReverse = new StringBuilder(s).reverse().toString();
+        int[][] memo = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        return n - lcs(s, sReverse, n, n, memo);
+    }
+
+    public static void main(String[] args) {
+        String s = "zjveiiwvc";
+        System.out.println(minInsertions(s));
+    }
+}
